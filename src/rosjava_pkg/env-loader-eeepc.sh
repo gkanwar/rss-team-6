70daaa176e00029d6112b302f777bf3ca2bc7b40
RSS Student <rss-team6@lists.csail.mit.edu>
2014-04-03 15:44:39 -0400

    Added env loader for eeepc, add config test files, fix merge conflict in settings.gradle

--- src/rosjava_pkg/env-loader-eeepc.sh
+++ src/rosjava_pkg/env-loader-eeepc.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+ #!/bin/bash
 
 echo "Env loader"
 
 export ROS_MASTER_URI="http://eeepc2:11311"
-export ROS_HOSTNAME="workstation"
+export ROS_HOSTNAME="eeepc2"
 
 export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb, width=160, height=120 ! ffmpegcolorspace"
 export DISPLAY=":0"
