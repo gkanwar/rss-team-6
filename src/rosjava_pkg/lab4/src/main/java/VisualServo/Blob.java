4e588afcb1e2c6986c1edf42eae942f5052c2200
Katharine Daly <dalyk@mit.edu>
2014-04-06 16:26:40 -0400

    added printouts to blobtrackingchallenge

--- src/rosjava_pkg/lab4/src/main/java/VisualServo/Blob.java
+++ src/rosjava_pkg/lab4/src/main/java/VisualServo/Blob.java
@@ -3,39 +3,38 @@ package VisualServo;
 import java.awt.geom.Point2D;
 import java.util.Set;
 
-public class Blob{
-    private Set<Point2D.Double> points;
-    private int[][] hues;
-    private double centroidX;
-    private double centroidY;
-    private double width;
-    private double height;
-    private double hue;
-    private boolean isSphere;
-    
-    private BlobColor color;
-    private double range;
-    private double bearing;
-    
-	
-    public static enum BlobColor {
-    	RED(0,20), GREEN(70,100), BLUE(150,220);
-    	
-    	public final int minHue;
-    	public final int maxHue;
-    	
-    	private BlobColor(int minHue, int maxHue) {
-    		this.minHue = minHue;
-    		this.maxHue = maxHue;
-    	}
-    }
-    
+public class Blob {
+	private Set<Point2D.Double> points;
+	private int[][] hues;
+	private double centroidX;
+	private double centroidY;
+	private double width;
+	private double height;
+	private double hue;
+	private boolean isSphere;
+
+	private BlobColor color;
+	private double range;
+	private double bearing;
+
+	public static enum BlobColor {
+		RED(0, 20), GREEN(70, 100), BLUE(150, 220);
+
+		public final int minHue;
+		public final int maxHue;
+
+		private BlobColor(int minHue, int maxHue) {
+			this.minHue = minHue;
+			this.maxHue = maxHue;
+		}
+	}
+
 	public Blob(Set<Point2D.Double> points, int[][] hues) {
 		this.points = points;
 		this.hues = hues;
-		calculateBasics();		
+		calculateBasics();
 	}
-	
+
 	private void calculateBasics() {
 		double sumX = 0;
 		double minX = width;
@@ -44,7 +43,7 @@ public class Blob{
 		double minY = height;
 		double maxY = 0;
 		double sumHue = 0;
-		
+
 		for (Point2D.Double point : points) {
 			sumX += point.x;
 			sumY += point.y;
@@ -54,27 +53,27 @@ public class Blob{
 			maxY = Math.max(maxY, point.y);
 			sumHue += hues[(int) point.y][(int) point.x];
 		}
-		
+
 		centroidX = sumX / points.size();
 		centroidY = sumY / points.size();
 		width = maxX - minX;
 		height = maxY - minY;
 		hue = sumHue / points.size();
-		
+
 		calculateRange();
 		calculateBearing();
 		calculateColor();
 		calculateShape();
 	}
-	
+
 	private void calculateRange() {
-		
+
 	}
-	
+
 	private void calculateBearing() {
-		
+
 	}
-	
+
 	private void calculateColor() {
 		for (BlobColor potentialColor : BlobColor.values()) {
 			if (hue >= potentialColor.minHue && hue <= potentialColor.maxHue) {
@@ -82,27 +81,27 @@ public class Blob{
 			}
 		}
 	}
-	
+
 	private void calculateShape() {
-		
+
 	}
-	
+
 	public Set<Point2D.Double> getPoints() {
 		return points;
 	}
-	
+
 	public double getRange() {
 		return range;
 	}
-	
+
 	public double getBearing() {
 		return bearing;
 	}
-	
+
 	public BlobColor getColor() {
 		return color;
 	}
-	
+
 	public boolean getIsSphere() {
 		return isSphere;
 	}
